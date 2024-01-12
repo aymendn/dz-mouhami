b85940cf176f99b603743714e2760b52c520a29f
Aymen Denoub <64363186+aymendn@users.noreply.github.com>
2024-01-12 22:29:25 +0100

    Add react-rating package and LeaveReviewDialog component

---
+++ client/src/components/LeaveReviewDialog.js
@@ -0,0 +1,68 @@
+import Rating from "react-rating";
+import Dialog from "./Dialog";
+import starFilled from "../assets/star_filled.svg";
+import star from "../assets/star.svg";
+
+const LeaveReviewDialog = ({ trigger }) => {
+  return (
+    <Dialog trigger={trigger}>
+      {(close) => (
+        <div className="flex flex-col gap-12 w-full items-start max-w-[600px] mx-auto">
+          <div className="flex flex-col gap-3 w-full mx-auto items-center">
+            <div className="text-center text-2xl font-bold text-[#094b72]">
+              Leave a Review
+            </div>
+            <div className="text-center text-sm text-[#496c80] ml-4 md:w-5/6">
+              Please share your experience with us and help us improve our
+              services.
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-8 w-full items-start">
+            <div className="flex flex-col gap-2 w-full items-start">
+              <div className="opacity-80 text-sm font-medium text-[#094b72] ml-1">
+                Rating
+              </div>
+              <Rating
+                emptySymbol={<img src={star} className="icon" width={36} />}
+                fullSymbol={
+                  <img src={starFilled} className="icon" width={36} />
+                }
+              />
+            </div>
+            {/* Repeat the above structure for the Deuxieme Créneau section */}
+          </div>
+
+          <div className="flex flex-col gap-8 w-full font-['DM_Sans'] items-start">
+            <div className="flex flex-col gap-2 w-full items-start">
+              <div className="opacity-80 text-sm font-medium text-[#094b72] ml-1">
+                Comment
+              </div>
+              <textarea
+                className="text-sm text-[#a6bdcb] border-solid border-[#d8ebf6] bg-white flex flex-row w-full min-h-[100px] items-start  border-2 rounded-xl py-2 px-4"
+                placeholder="Additional Notes or Comments...."
+              ></textarea>
+            </div>
+
+            <div className="flex flex-row gap-5 w-full items-start max-w-xl mx-auto">
+              <button
+                onClick={close}
+                className="text-center text-sm font-medium text-[#09283a] bg-[#e5f2fa] flex flex-row justify-center items-center px-6 py-3 rounded-full w-full hover:bg-[#d8ebf6]"
+              >
+                Annuler
+              </button>
+              <button
+                onClick={close}
+                className="text-center text-sm font-medium text-[#f5fbff] bg-[#094b72] flex flex-row justify-center items-center px-6 py-3 rounded-full w-full hover:opacity-90"
+              >
+                Confirmer
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </Dialog>
+  );
+};
+
+export default LeaveReviewDialog;
--- client/src/pages/Lawyer/Reviews.js
+++ client/src/pages/Lawyer/Reviews.js
@@ -1,4 +1,5 @@
 import ReviewsIcon from "../../assets/reviews.svg";
+import LeaveReviewDialog from "../../components/LeaveReviewDialog";
 import ReviewCard from "../../components/ReviewCard";
 
 const Reviews = () => {
@@ -16,9 +17,19 @@ const Reviews = () => {
 
   return (
     <>
-      <div className="flex items-center gap-1">
-        <img src={ReviewsIcon} alt="Locationon" className="w-4" />
-        <div className="font-medium text-[#0f3146]">Reviews</div>
+      <div className="flex flex-row justify-between items-center border-b border-[#e4e4e4] pb-4 mb-4">
+        <div className="flex items-center gap-2">
+          <img src={ReviewsIcon} alt="Locationon" className="w-5" />
+          <div className="font-medium text-lg text-[#0f3146]">Reviews</div>
+        </div>
+        <LeaveReviewDialog
+          key="appointment"
+          trigger={
+            <div className="bg-[#094b72] text-white text-sm font-medium  py-2 px-5 rounded-full hover:opacity-90 cursor-pointer transition-all ms-2">
+              Leave a review
+            </div>
+          }
+        />
       </div>
       {/* review cards here */}
       <div className="flex flex-col gap-10 mt-4">
