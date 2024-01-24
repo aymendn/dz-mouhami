c67997beae73731971cc7aeb61bbdd82c3a57d18
Bensmail-anis <a_bensmail@estin.dz>
2024-01-24 14:49:53 +0100

    fixing our last meeting points

--- server/core/admin.py
+++ server/core/admin.py
@@ -1,10 +1,30 @@
 from django.contrib import admin
-from .models import LawyerProfile
+from .models import LawyerProfile , ClientProfile
 
 @admin.register(LawyerProfile)
 class LawyerProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'specialization', 'phone_number', 'address', 'language', 'approved']
     list_filter = ['approved']
+    search_fields = ['user__first_name', 'user__last_name', 'user__email']
+
+    @admin.display(ordering='user__first_name')
+    def first_name(self, obj):
+        return obj.user.first_name
+
+    @admin.display(ordering='user__last_name')
+    def last_name(self, obj):
+        return obj.user.last_name
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('user')
+
+    class Meta:
+        ordering = ['user__first_name', 'user__last_name']
+
+
+@admin.register(ClientProfile)
+class ClientProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'phone_number', 'address']
     search_fields = ['user__first_name', 'user__last_name', 'user__email']
 
     @admin.display(ordering='user__first_name')
--- server/core/serializers.py
+++ server/core/serializers.py
@@ -1,22 +1,31 @@
 # serializers.py
 from rest_framework import serializers
-from .models import Address, LawyerProfile, LawyerImage, LawyerDocument , ClientProfile , User
+from .models import Address, LawyerProfile, LawyerImage, LawyerDocument , ClientProfile , User , TimeSlot
+
 
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
         fields = ['id', 'street', 'city', 'state', 'zip_code', 'country']
 
+class TimeSlotSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TimeSlot
+        fields = ['id', 'day', 'start_time', 'end_time']
+
 
 class LawyerProfileSerializer(serializers.ModelSerializer):
     address = AddressSerializer(required=False, allow_null=True)
+    time_slots = TimeSlotSerializer(required=False, allow_null=True, many=True)
+    rating = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = LawyerProfile
-        fields = ['id', 'specialization', 'phone_number', 'bio', 'language', 'address']
+        fields = ['id', 'specialization', 'phone_number', 'bio', 'language', 'address', 'time_slots' , 'rating']
 
     def create(self, validated_data):
         address_data = validated_data.pop('address', None)
+        time_slots_data = validated_data.pop('time_slots', None)
 
         if address_data:
             address = Address.objects.create(**address_data)
@@ -24,7 +33,46 @@ class LawyerProfileSerializer(serializers.ModelSerializer):
 
         lawyer = LawyerProfile.objects.create(**validated_data)
 
-        return lawyer
+        if time_slots_data:
+            for slot_data in time_slots_data:
+                TimeSlot.objects.create(lawyer=lawyer, **slot_data)
+
+        # Include serialized TimeSlots in the response
+        lawyer_time_slots = TimeSlot.objects.filter(lawyer=lawyer)
+        time_slots_serializer = TimeSlotSerializer(lawyer_time_slots, many=True)
+        validated_data['time_slots'] = time_slots_serializer.data
+
+        return validated_data
+
+    def update(self, instance, validated_data):
+        instance.specialization = validated_data.get('specialization', instance.specialization)
+        instance.phone_number = validated_data.get('phone_number', instance.phone_number)
+        instance.bio = validated_data.get('bio', instance.bio)
+        instance.language = validated_data.get('language', instance.language)
+
+        # Update Address
+        address_data = validated_data.get('address')
+        if address_data:
+            instance.address.street = address_data.get('street', instance.address.street)
+            instance.address.city = address_data.get('city', instance.address.city)
+            instance.address.state = address_data.get('state', instance.address.state)
+            instance.address.zip_code = address_data.get('zip_code', instance.address.zip_code)
+            instance.address.country = address_data.get('country', instance.address.country)
+            instance.address.save()
+
+        # Update TimeSlots
+        time_slots_data = validated_data.get('time_slots')
+        if time_slots_data:
+            instance.time_slots.all().delete()
+            for slot_data in time_slots_data:
+                TimeSlot.objects.create(lawyer=instance, **slot_data)
+
+        instance.save()
+        return instance
+
+
+
+
 
 
 class LawyerImageSerializer(serializers.ModelSerializer):
@@ -64,4 +112,18 @@ class ClientProfileSerializer(serializers.ModelSerializer):
 
         client_profile = ClientProfile.objects.create(**validated_data)
         return client_profile
+    
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
 
+class LawyerProfileAdminListSerializer(serializers.ModelSerializer):
+    images = LawyerImageSerializer(many=True , read_only=True)
+    documents = LawyerDocumentSerializer(many=True , read_only=True)
+    first_name = serializers.CharField(source='user.first_name' , read_only=True)
+    last_name = serializers.CharField(source='user.last_name' , read_only=True)
+
+    class Meta:
+        model = LawyerProfile
+        fields = ['id','first_name', 'last_name', 'specialization', 'images', 'documents' , 'approved']
