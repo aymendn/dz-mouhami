c2ded0342b4db0a9dd9328225c437378b8abde32
Ms <125511982+mayamelissa15@users.noreply.github.com>
2024-01-26 00:42:07 +0000

    Trying link editProfil

--- client/src/pages/EditProfile/EditProfilePage.js
+++ client/src/pages/EditProfile/EditProfilePage.js
@@ -5,6 +5,8 @@ import { useState } from "react";
 import SelectField from "../../components/SelectField";
 import Footer from "../../components/Footer";
 import { useTranslation } from "react-i18next";
+import axios from "axios";
+
 const EditProfilePage = () => {
   const { t } = useTranslation();
 
@@ -16,11 +18,42 @@ const EditProfilePage = () => {
     setSelectedImage(file);
   };
 
+  const [data, setData] = useState(
+    {
+     age: null,
+     gender:"",
+     phone_number:null,
+     street:"",
+     city:"",
+     state:"",
+     zip_code:null,
+     country:"",
+    }
+   );
+
   const handleImageClick = () => {
     // Trigger the file input click when the image is clicked
     document.getElementById("imageInput").click();
   };
 
+  const handleChange = (e) =>{
+
+    setData(prev=>({...prev,[e.target.name]: e.target.value}))
+   
+} 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const apiUrl = 'http://127.0.0.1:8000/core/dashboard/{id}/';
+  
+    axios.put(apiUrl, data)
+      .then(response => {
+        
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('Erreur :', error);
+      });
+  };
   return (
     <div className="flex flex-col min-h-screen">
       <div className="z-50">
@@ -66,6 +99,7 @@ const EditProfilePage = () => {
           name={"age"} 
           label={"age"} 
           placeholder={"45"} 
+          onChange={handleChange}
            />
 
           <SelectField
@@ -76,25 +110,25 @@ const EditProfilePage = () => {
                 { value: "Femme", label: "Femme" },
                 { value: "homme", label: "homme" },
               ]}
-              
+              onChange={handleChange}
             />
-            <TextField id={"phone"} name={"phone_number"} label={"Phone"} type={"number"} placeholder={"06 00 00 00 00"} />
+            <TextField id={"phone"} name={"phone_number"} label={"Phone"} type={"number"} placeholder={"06 00 00 00 00"} onChange={handleChange}/>
 
 
-          <TextField id={"street"} name={"street"} label={"street"}  />
+          <TextField id={"street"} name={"street"} label={"street"}  onChange={handleChange}/>
 
-          <TextField id={"city"} label={"city"} placeholder={"Paris"} />
+          <TextField id={"city"} label={"city"} placeholder={"Paris"} onChange={handleChange}/>
 
-          <TextField id={"state"} name={"state"} label={"state"}  />
+          <TextField id={"state"} name={"state"} label={"state"}  onChange={handleChange}/>
 
-          <TextField id={"zip_code"} name={"zip_code"} label={"zip code"} type={"number"} />
+          <TextField id={"zip_code"} name={"zip_code"} label={"zip code"} type={"number"} onChange={handleChange}/>
 
-          <TextField id={"country"} name={"country"} label={"country"}  />
+          <TextField id={"country"} name={"country"} label={"country"}  onChange={handleChange}/>
         </div>
 
 
           <div className="flex justify-end m-4">
-            <button className="transition-all hover:opacity-90 mx-4 border-1 bg-[#094B72] py-3 px-8 rounded-3xl text-white font-normal text-md flex gap-2">
+            <button onClick={handleSubmit} className="transition-all hover:opacity-90 mx-4 border-1 bg-[#094B72] py-3 px-8 rounded-3xl text-white font-normal text-md flex gap-2">
               <p>{t("save")}</p>
             </button>
           </div>
