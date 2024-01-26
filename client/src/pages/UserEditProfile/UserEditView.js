f136c53dde281b4ce35f34ed956014f102a23d0c
Ms <125511982+mayamelissa15@users.noreply.github.com>
2024-01-26 00:52:53 +0000

    Update UserEditView.js

--- client/src/pages/UserEditProfile/UserEditView.js
+++ client/src/pages/UserEditProfile/UserEditView.js
@@ -3,7 +3,7 @@ import profileIcon from "../../assets/profile.svg";
 import TextField from "../../components/TextField";
 import SelectField from "../../components/SelectField";
 import { useTranslation } from "react-i18next";
-
+import TextAreaField from "../../components/TextAreaField";
 const UserEditView = () => {
   const { t } = useTranslation();
 
@@ -58,27 +58,63 @@ const UserEditView = () => {
         )}
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 m-8">
-        <TextField id={"name"} label={t("lastName")} placeholder={"John"} />
+        
+        
+         <TextField
+              id={"phone_number"}
+              name={"phone_number"}
+              label={"phone number"}
+              placeholder={"555-222-333"}
+            />
 
-        <TextField id={"surname"} label={t("firstName")} placeholder={"Doe"} />
-        <TextField id={"name"} label={t("lastName")} placeholder={"John"} />
 
-        <TextField id={"surname"} label={t("firstName")} placeholder={"Doe"} />
+            <TextField 
+            id={"street"}  
+            label={"street"}
+            name={"street"}  
+             />
+            <SelectField
+              id={"specialization"}
+              name={"specialization"}
+              label={"Legal specialization"}
+              placeholder={"Select a specialization"}
+              options={[
+                { value: "Agression", label: "Agression" },
+                { value: "Crime", label: "Crime" },
+                { value: "Robbing", label: "Robbing" },
+              ]}
+            />
+            <TextField 
+            id={"city"}  
+            label={"city"}
+            name={"city"}  
+             />
+            <TextField 
+            id={"state"}  
+            label={"state"}
+            name={"state"} 
+             
+             />
 
-        <SelectField
-          id={"Specification"}
-          label={"Legal Specification"}
-          placeholder={"Select a specification"}
-          options={[
-            { value: "Agression", label: "Agression" },
-            { value: "Crime", label: "Crime" },
-            { value: "Robbing", label: "Robbing" },
-          ]}
-        />
+            <TextField 
+            id={"country"}  
+            label={"country"}
+            name={"country"}  
+             />
 
-        <TextField id={"surname"} label={t("firstName")} placeholder={"Doe"} />
-      </div>
-
+          <TextField 
+            id={"zip_code"}  
+            label={"zip code"}
+            name={"zip_code"}  
+            type={"number"}
+             />
+            <TextAreaField
+            id={"bio"}  
+            label={"bio"}
+            name={"bio"}
+            />
+          
+        </div>
       <div className="flex justify-end m-4">
         <button className="transition-transform transform hover:opacity-90 duration-500  mx-4 border-1 bg-[#094B72] py-3 px-8 rounded-3xl text-white font-normal text-md flex gap-2">
           <p>{t("save")}</p>
