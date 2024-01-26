430b87b8b79583953abb90c3802505bfffdf7714
Yasser Messahli <y_messahli@estin.dz>
2024-01-26 16:52:40 +0100

    change LawyerProfile model fields

--- server/core/models.py
+++ server/core/models.py
@@ -51,9 +51,9 @@ class ClientProfile(models.Model):
 # LawyerProfile model, using OneToOneField for a one-to-one relationship with User
 class LawyerProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE , related_name='lawyer_profile')
-    specialization = models.CharField(max_length=255)
+    specialization = models.CharField(max_length=1000)
     phone_number = models.CharField(max_length=500)
-    bio = models.CharField(max_length=255)
+    bio = models.CharField(max_length=1000)
     address = models.ForeignKey(Address, on_delete=models.CASCADE , related_name='lawyer_address')
     language = models.CharField(max_length=255)
     approved = models.BooleanField(null = True , blank = True)
